a45c103fed82774ba26266cf8afbca2708b2b5ce
Joshua Downer <joshua.downer@gmail.com>
2013-10-16 05:48:26 -0400

    app: Removed logging setup
    This should be performed in the driver script (3d-viewer in this case)

--- app.py
+++ app.py
@@ -17,23 +17,6 @@ import OpenGL.GL.shaders as glsl
 
 import core
 
-LOGLEVEL = { 
-        'debug':    logging.DEBUG,
-        'info':     logging.INFO,
-        'warning':  logging.WARNING,
-        'error':    logging.ERROR,
-        'critical': logging.CRITICAL}
-
-log = logging.getLogger(__name__)
-formatter = logging.Formatter('%(asctime)s:%(levelname)s: %(message)s')
-sh = logging.StreamHandler()
-sh.setFormatter(formatter)
-log.addHandler(sh)
-
-def set_log_level(level):
-    log.setLevel(LOGLEVEL[level.lower()])
-
-
 class App(object):
     def __init__(self):
         self._renderer = core.Renderer(800, 600)
